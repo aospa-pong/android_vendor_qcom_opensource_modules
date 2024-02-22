RealJohnGalt/GaltsGulch-sm8150

display: translate PCC to CSC
PCC cannot be applied per-plane, but CSC can. Translate PCC to CSC
to allow skipping it for some planes.

Change-Id: I76fa04fed6bcc9426cf2a38c58c23e49e27b8750

[@RealJohnGalt] adapt for k4.14

drivers/gpu/drm/msm/sde/sde_color_processing.c
@@ -24,6 +24,7 @@
 #include "sde_ad4.h"
 #include "sde_hw_interrupts.h"
 #include "sde_core_irq.h"
+#include "sde_plane.h"
 #include "dsi_panel.h"
 
 #ifdef CONFIG_DRM_MSM_KCAL_CTRL
@@ -649,6 +650,9 @@ static void sde_cp_crtc_setfeature(struct sde_cp_node *prop_node,
 	sde_cp_get_hw_payload(prop_node, &hw_cfg, &feature_enabled);
 	hw_cfg.num_of_mixers = sde_crtc->num_mixers;
 	hw_cfg.last_feature = 0;
+
+	if (prop_node->feature == SDE_CP_CRTC_DSPP_PCC)
+		return;
 
 	for (i = 0; i < num_mixers && !ret; i++) {
 		hw_lm = sde_crtc->mixers[i].hw_lm;
@@ -2137,4 +2141,25 @@ int sde_cp_hist_interrupt(struct drm_crtc *crtc_drm, bool en,
 
 exit:
 	return ret;
+}
+
+const struct drm_msm_pcc *sde_cp_crtc_get_pcc_cfg(struct drm_crtc *drm_crtc)
+{
+	struct drm_property_blob *blob = NULL;
+	struct sde_cp_node *prop_node = NULL;
+	struct sde_crtc *crtc;
+
+	crtc = to_sde_crtc(drm_crtc);
+
+	mutex_lock(&crtc->crtc_cp_lock);
+	list_for_each_entry(prop_node, &crtc->feature_list, feature_list) {
+		if (prop_node->feature == SDE_CP_CRTC_DSPP_PCC) {
+			blob = prop_node->blob_ptr;
+			break;
+		}
+	}
+
+	mutex_unlock(&crtc->crtc_cp_lock);
+
+	return blob ? blob->data : NULL;
 }

aospa-pong/android_vendor_qcom_opensource_modules

display-drivers/msm/sde/sde_color_processing.c
@@ -12,6 +12,7 @@
 #include "sde_color_processing.h"
 #include "sde_kms.h"
 #include "sde_crtc.h"
+#include "sde_plane.h"
 #include "sde_hw_dspp.h"
 #include "sde_hw_lm.h"
 #include "sde_ad4.h"
@@ -1679,6 +1680,9 @@ static void _sde_cp_crtc_commit_feature(struct sde_cp_node *prop_node,
 			continue;
 		hw_cfg.dspp[i] = hw_dspp;
 	}
+
+	if (prop_node->feature == SDE_CP_CRTC_DSPP_PCC)
+		return;
 
 	if ((prop_node->feature >= SDE_CP_CRTC_MAX_FEATURES) ||
 			set_crtc_feature_wrappers[prop_node->feature] == NULL) {
@@ -4980,3 +4984,23 @@ void sde_cp_set_skip_blend_plane_info(struct drm_crtc *drm_crtc,
 	mutex_unlock(&crtc->crtc_cp_lock);
 }
 
+const struct drm_msm_pcc *sde_cp_crtc_get_pcc_cfg(struct drm_crtc *drm_crtc)
+{
+	struct drm_property_blob *blob = NULL;
+	struct sde_cp_node *prop_node = NULL;
+	struct sde_crtc *crtc;
+
+	crtc = to_sde_crtc(drm_crtc);
+
+	mutex_lock(&crtc->crtc_cp_lock);
+	list_for_each_entry(prop_node, &crtc->cp_feature_list, cp_feature_list) {
+		if (prop_node->feature == SDE_CP_CRTC_DSPP_PCC) {
+			blob = prop_node->blob_ptr;
+			break;
+		}
+	}
+
+	mutex_unlock(&crtc->crtc_cp_lock);
+
+	return blob ? blob->data : NULL;
+}